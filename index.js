730cce99d1b75ab7549eb4d4e38755febae92c54
unknown <irvanngalam32@example.com>
2026-04-27 03:23:18 +0700

    add discord bot features

--- index.js
+++ index.js
@@ -478,3 +478,26 @@ client.on('messageReactionRemove', async (reaction, user) => {
 });
 
 client.login(process.env.TOKEN);
+
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot online sebagai ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong dari Playzone!');
+  }
+});
+
+client.login(process.env.TOKEN);
